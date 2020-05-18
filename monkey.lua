44889f359393bbc206604af17680628b8f8440fa
4v0v <30574115+4v0v@users.noreply.github.com>
2020-05-18 03:45:50 +0200

    Create monkey.lua

---
+++ monkey.lua
@@ -0,0 +1,26 @@
+table.insert(package.loaders, 1, function(name)
+	local name = name:gsub("%.", "/") .. ".lua"
+    local file = love.filesystem.read(name)
+    if not file then return nil end
+
+    local var = "([%w%.:_%[%]'\"]+)"
+	for i, v in ipairs(
+        {
+            { pattern = var .. "%s*%+=" , replacement = "%1 = %1 + "},
+            { pattern = var .. "%s*%-=" , replacement = "%1 = %1 - "},
+            { pattern = var .. "%s*%*=" , replacement = "%1 = %1 * "},
+            { pattern = var .. "%s*/="  , replacement = "%1 = %1 / "},
+            { pattern = var .. "%s*^="  , replacement = "%1 = %1 ^ "},
+            { pattern = var .. "%s*%%=" , replacement = "%1 = %1 %% "},
+            { pattern = var .. "%s*%..=", replacement = "%1 = %1 .. "},
+            { pattern = var .. "%+%+"   , replacement = "%1 = %1 + 1"},
+            { pattern = "&&"            , replacement = " and "},
+            { pattern = "||"            , replacement = " or "},
+            { pattern = "!="            , replacement = "~="},
+            { pattern = "!"             , replacement = " not "},
+        }
+    ) do 
+		file = file:gsub(v.pattern, v.replacement) 
+	end
+	return assert(loadstring(file, name))
+end)
