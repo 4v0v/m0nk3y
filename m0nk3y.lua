8ec832df0b9e290374f0284f18ec901bc644e54c
4v0v <30574115+4v0v@users.noreply.github.com>
2020-06-28 00:01:16 +0200

    Update m0nk3y.lua

--- m0nk3y.lua
+++ m0nk3y.lua
@@ -4,23 +4,23 @@ table.insert(package.loaders, 2, function(name)
     if not file then return nil end
 
     local var = "([%w%.:_%[%]'\"]+)"
-	for i, v in ipairs(
-        {
-            { pattern = var .. "%s*%+=" , replacement = "%1 = %1 + "},
-            { pattern = var .. "%s*%-=" , replacement = "%1 = %1 - "},
-            { pattern = var .. "%s*%*=" , replacement = "%1 = %1 * "},
-            { pattern = var .. "%s*/="  , replacement = "%1 = %1 / "},
-            { pattern = var .. "%s*^="  , replacement = "%1 = %1 ^ "},
-            { pattern = var .. "%s*%%=" , replacement = "%1 = %1 %% "},
-            { pattern = var .. "%s*%..=", replacement = "%1 = %1 .. "},
-            { pattern = var .. "%+%+"   , replacement = "%1 = %1 + 1"},
-            { pattern = "&&"            , replacement = " and "},
-            { pattern = "||"            , replacement = " or "},
-            { pattern = "!="            , replacement = "~="},
-            { pattern = "!"             , replacement = " not "},
-	    { pattern = "([%s,={%(])fn%(" , replacement = "%1function("},
-        }
-    ) do 
+	for i, v in ipairs({
+		{ pattern = var .. "%s*%+=" , replacement = "%1 = %1 + "},
+            	{ pattern = var .. "%s*%-=" , replacement = "%1 = %1 - "},
+            	{ pattern = var .. "%s*%*=" , replacement = "%1 = %1 * "},
+            	{ pattern = var .. "%s*/="  , replacement = "%1 = %1 / "},
+            	{ pattern = var .. "%s*^="  , replacement = "%1 = %1 ^ "},
+            	{ pattern = var .. "%s*%%=" , replacement = "%1 = %1 %% "},
+            	{ pattern = var .. "%s*%..=", replacement = "%1 = %1 .. "},
+            	{ pattern = var .. "%+%+"   , replacement = "%1 = %1 + 1"},
+            	{ pattern = "&&"            , replacement = " and "},
+	 	{ pattern = "||"            , replacement = " or "},
+		{ pattern = "!="            , replacement = "~="},
+		{ pattern = "!"             , replacement = " not "},
+		{ pattern = "([%s,={%(])fn%(" , replacement = "%1function("},
+    		{ pattern = "for%spairs%("  , replacement = "for key, value in pairs("},
+    		{ pattern = "for%sipairs%(" , replacement = "for index, value in ipairs("},
+        }) do 
 		file = file:gsub(v.pattern, v.replacement) 
 	end
 	return assert(loadstring(file, name))
