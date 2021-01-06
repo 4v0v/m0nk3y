7edf2eb89dc3b8b029f5ff845ff37165ad5ad580
4v0v <30574115+4v0v@users.noreply.github.com>
2021-01-06 02:10:32 +0100

    Update m0nk3y.lua

--- m0nk3y.lua
+++ m0nk3y.lua
@@ -25,8 +25,11 @@ local function parse(s)
 		{ patt = "fn%("               , repl = "function("},
 		{ patt = "for" .. wl .. integer .. wl .. "do", repl = "for it = 1, %1 do"},
 		{ patt = "for" .. wl .. expression .. wl .. "do", repl = "for key, it in pairs(%1) do"},
+		{ patt = "ifor" .. wl .. expression .. wl .. "do", repl = "for key, it in ipairs(%1) do"},
 		{ patt = "for" .. wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for key, %1 in pairs(%2) do"},
+		{ patt = "ifor" .. wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for key, %1 in ipairs(%2) do"},
 		{ patt = "for" .. wl .. simple_var .. "," .. _wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for %1, %2 in pairs(%3) do"},
+		{ patt = "ifor" .. wl .. simple_var .. "," .. _wl .. simple_var .. wl .. "in" .. wl .. var .. wl .. "do", repl = "for %1, %2 in ipairs(%3) do"},
 	}
 
 	for i, v in ipairs(patterns) do str = str:gsub(v.patt, v.repl) end
@@ -38,5 +41,5 @@ table.insert(package.loaders, 2, function(name)
 	local file        = love.filesystem.read(name)
 	local parsed_file = parse(file)
 
-	return assert(loadstring(parsed_file))
+	return assert(loadstring(parsed_file, name))
 end)
